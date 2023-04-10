fd80b0b762b9a52ee034f0868038323ec1549596
Sander <meije702@hotmail.com>
2023-04-10 21:35:58 +0200

    strip price id

--- constants.ts
+++ constants.ts
@@ -4,6 +4,6 @@ export const SITE_DESCRIPTION = "Let's make America grade again!";
 
 export const AUTHENTICATED_REDIRECT_PATH = "/dashboard";
 
-export const STRIPE_PREMIUM_PLAN_PRICE_ID = "price_1MPiEkD6QJts4RaYcp1SevPe";
+export const STRIPE_PREMIUM_PLAN_PRICE_ID = "price_1MvP5mAGOs6hrAsHiw4ynEmi";
 
 export const FREE_PLAN_TODOS_LIMIT = 3;
