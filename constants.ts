f4863a3dc7392619ad846318f1f064b04a295563
Sander <meije702@hotmail.com>
2023-04-10 19:44:12 +0200

    names and such

--- constants.ts
+++ constants.ts
@@ -1,9 +1,9 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
-export const SITE_NAME = "Your SaaS";
-export const SITE_DESCRIPTION = "Some details about your SaaS.";
+export const SITE_NAME = "Fieldgrader";
+export const SITE_DESCRIPTION = "Let's make America grade again!";
 
 export const AUTHENTICATED_REDIRECT_PATH = "/dashboard";
 
 export const STRIPE_PREMIUM_PLAN_PRICE_ID = "price_1MPiEkD6QJts4RaYcp1SevPe";
 
-export const FREE_PLAN_TODOS_LIMIT = 5;
+export const FREE_PLAN_TODOS_LIMIT = 3;
